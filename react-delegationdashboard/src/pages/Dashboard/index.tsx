2eecbe9557503707bba84b7d760b917b7e675958
Mihai Eremia <jobs@dani.ro>
2021-03-23 10:17:14 +0000

    Revert "Merge pull request #72 from ElrondNetwork/Fix-display-unbound-period"
    This reverts commit 8a2d21b8f83fbb4061706fdffd3ca42ed9cd69b9, reversing
    changes made to 379d54f10fc0259a593a8a00904ea1867405b0f0.

--- react-delegationdashboard/src/pages/Dashboard/index.tsx
+++ react-delegationdashboard/src/pages/Dashboard/index.tsx
@@ -4,11 +4,9 @@ import Delegation from './Delegation';
 import PendingUndelegated from './PendingUndelegated';
 import { Redirect } from 'react-router-dom';
 import Overview from 'components/Overview';
-import State from 'components/State';
-import { faCircleNotch } from '@fortawesome/free-solid-svg-icons';
 
 const Dashboard = () => {
-  const { loggedIn, networkConfig } = useContext();
+  const { loggedIn } = useContext();
 
   if (!loggedIn) {
     return <Redirect to="/" />;
@@ -17,14 +15,10 @@ const Dashboard = () => {
     <div className="dashboard w-100">
       <div className="card border-0">
         <Overview />
-        {networkConfig.roundDuration === -1 ? (
-          <State icon={faCircleNotch} iconClass="fa-spin text-primary" />
-        ) : (
-          <div className="card-body pt-0 px-spacer pb-spacer">
-            <Delegation />
-            <PendingUndelegated />
-          </div>
-        )}
+        <div className="card-body pt-0 px-spacer pb-spacer">
+          <Delegation />
+          <PendingUndelegated />
+        </div>
       </div>
     </div>
   );
