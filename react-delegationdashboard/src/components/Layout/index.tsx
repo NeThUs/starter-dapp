37ca6816aebb6884162338034a240c6db4b0c379
Mihai Eremia <jobs@dani.ro>
2021-03-31 15:04:56 +0100

    New APR

--- react-delegationdashboard/src/components/Layout/index.tsx
+++ react-delegationdashboard/src/components/Layout/index.tsx
@@ -146,7 +146,7 @@ const Layout = ({ children, page }: { children: React.ReactNode; page: string })
             .toString();
           dispatch({
             type: 'setAprPercentageAfterFee',
-            aprPercentageAfterFee: '19.5',
+            aprPercentageAfterFee: '18.06',
           });
         }
       )
