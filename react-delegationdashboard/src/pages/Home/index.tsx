654c766bd4b3242cd5d5395529b2dbbe4c3ce2a4
NeThUs <78180290+NeThUs@users.noreply.github.com>
2022-01-04 22:02:31 +0200

    Update index.tsx

--- react-delegationdashboard/src/pages/Home/index.tsx
+++ react-delegationdashboard/src/pages/Home/index.tsx
@@ -26,15 +26,6 @@ const Home = () => {
       ) : loading ? (
         <State icon={faCircleNotch} iconClass="fa-spin text-primary" />
       ) : (
-        <header class="sticky-top bg-white" style="border-bottom: 1px solid #999999;">	
-        <div class="container">
-        <nav class="navbar bg-white">
-          <a class="logo" href="#" target="_blank"></a>
-          <a href="#" id="aboutus" class="btn btn-xs login-btn-desktop hidden-xs slideLeft">About Us </a>
-          <a href="https://t.me/NTHIncAg" id="telegram" class="btn btn-xs login-btn-desktop hidden-xs slideLeft">Telegram Group </a>
-        </nav>
-        </div>	
-        </header>
         <div className="m-auto login-container">
         <div className="dashboard w-100">
          <div className="card border-0">
@@ -48,7 +39,7 @@ const Home = () => {
          <div className="card border-0">
           <div className="card-body pt-0 px-spacer pb-spacer">
            <p className="text-center">You can find us on telegram: </p>
-           <h4 className="text-center" id="telegram"><a href="https://t.me/NTHIncAg" target="_blank">NTH Inc</a></h4>
+           <h4 className="text-center"><a href="https://t.me/NTHIncAg" target="_blank">NTH Inc</a></h4>
           </div>
          </div>
         </div>
