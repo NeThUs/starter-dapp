dbb9061dcc4a2934d72975b7fe32794e5bd00d17
NeThUs <78180290+NeThUs@users.noreply.github.com>
2022-02-14 17:20:20 +0200

    Update config.mainnet.ts

--- react-delegationdashboard/src/config.mainnet.ts
+++ react-delegationdashboard/src/config.mainnet.ts
@@ -38,7 +38,7 @@ export const network: NetworkType = {
   apiAddress: 'https://api.elrond.com',
   gatewayAddress: 'https://gateway.elrond.com',
   explorerAddress: 'https://explorer.elrond.com/',
-  delegationContract: 'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqslllllsq4a3pq',
+  delegationContract: 'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqullllls6uyu9r',
 };
 
 const networkSchema = object({
